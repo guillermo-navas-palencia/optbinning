3895c140ace406d36db73535fdf3f005e40653c7
GUI <g.navas.palencia@gmail.com>
2020-01-24 02:30:48 +0100

    First test

--- tests/test_binning.py
+++ tests/test_binning.py
@@ -5,10 +5,35 @@ OptimalBinning testing.
 # Guillermo Navas-Palencia <g.navas.palencia@gmail.com>
 # Copyright (C) 2020
 
-from pytest import approx, raises
+import pandas as pd
+
+from pytest import raises
 
 from optbinning import OptimalBinning
+from sklearn.datasets import load_breast_cancer
+
+
+data = load_breast_cancer()
+df = pd.DataFrame(data.data, columns=data.feature_names)
+
+variable = "mean radius"
+x = df[variable].values
+y = data.target
 
 
 def test_params():
-    pass
+    with raises(TypeError):
+        optb = OptimalBinning(name=1)
+        optb.fit(x, y)
+
+    with raises(ValueError):
+        optb = OptimalBinning(dtype="nominal")
+        optb.fit(x, y)
+
+    with raises(ValueError):
+        optb = OptimalBinning(prebinning_method="new_method")
+        optb.fit(x, y)
+
+    with raises(ValueError):
+        optb = OptimalBinning(solver="new_solver")
+        optb.fit(x, y)

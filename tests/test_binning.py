698b9e6ccb47a8a0ec6c616c6483d3f0c1eeb0f1
GUI <g.navas.palencia@gmail.com>
2020-01-24 02:19:57 +0100

    Prepare tests CI

---
+++ tests/test_binning.py
@@ -0,0 +1,14 @@
+"""
+OptimalBinning testing.
+"""
+
+# Guillermo Navas-Palencia <g.navas.palencia@gmail.com>
+# Copyright (C) 2020
+
+from pytest import approx, raises
+
+from optbinning import OptimalBinning
+
+
+def test_params():
+    pass

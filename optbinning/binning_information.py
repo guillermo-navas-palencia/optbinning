304266cf530bfee071de631a735eee26a2e5bbf9
GUI <g.navas.palencia@gmail.com>
2020-01-26 07:07:23 +0100

    Update version

--- optbinning/binning_information.py
+++ optbinning/binning_information.py
@@ -20,7 +20,7 @@ except ImportError:
 
 def print_header():
     header = (
-        "optbinning (Version 0.1.0)\n"
+        "optbinning (Version 0.1.1)\n"
         "Copyright (c) 2019 Guillermo Navas-Palencia, Apache License 2.0\n")
 
     print(header)

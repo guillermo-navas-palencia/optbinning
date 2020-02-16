521a2557b50453800c2fec4d9d5d60e222a0cd34
GUI <g.navas.palencia@gmail.com>
2020-02-16 12:33:28 +0100

    Fix docstring definition of parameter verbose

--- optbinning/binning/continuous_binning.py
+++ optbinning/binning/continuous_binning.py
@@ -237,7 +237,7 @@ class ContinuousOptimalBinning(OptimalBinning):
     time_limit : int (default=100)
         The maximum time in seconds to run the optimization solver.
 
-    verbose : int or bool (default=False)
+    verbose : bool (default=False)
         Enable verbose output.
 
     Notes

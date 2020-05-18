f4178ce0d57e4cc8830dcf688d53ade32be194e7
GabrielSGoncalves <gabrielgoncalvesbr@gmail.com>
2020-05-18 10:07:23 -0300

    Change structure of plot functions to fit module standards.

--- optbinning/scorecard/plots.py
+++ optbinning/scorecard/plots.py
@@ -3,292 +3,302 @@ Module with plots for visualizing model performance.
 """
 
 # Gabriel S. Gonçalves <gabrielgoncalvesbr@gmail.com>
+# Guillermo Navas-Palencia <g.navas.palencia@gmail.com>
 # Copyright (C) 2020
+
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.metrics import roc_curve, roc_auc_score
+from sklearn.utils import check_array
+from sklearn.utils import check_consistent_length
+
+
+def _check_arrays(y, y_pred):
+    y = check_array(y, ensure_2d=False, force_all_finite=True)
+    y_pred = check_array(y_pred, ensure_2d=False, force_all_finite=True)
+
+    check_consistent_length(y, y_pred)
+
+    return y, y_pred
+
+
+def _check_parameters(title, xlabel, ylabel, savefig, fname):
+    if title is not None and not isinstance(title, str):
+        raise TypeError(
+            "title must be a string or None; got {}.".format(title)
+        )
+
+    if xlabel is not None and not isinstance(xlabel, str):
+        raise TypeError(
+            "xlabel must be a string or None; got {}.".format(xlabel)
+        )
+
+    if ylabel is not None and not isinstance(ylabel, str):
+        raise TypeError(
+            "ylabel must be a string or None; got {}.".format(ylabel)
+        )
+
+    if not isinstance(savefig, bool):
+        raise TypeError("savefig must be a boolean; got {}.".format(savefig))
+
+    if fname is not None and not isinstance(fname, str):
+        raise TypeError(
+            "fname must be a string or None; got {}.".format(fname)
+        )
+
+    if savefig is True and fname is None:
+        raise TypeError("fname must be provided if savefig is True.")
 
 
 def plot_auc_roc(
-    df,
-    target_col,
-    proba_col,
-    title="ROC curve",
-    xlabel="False positive rate",
-    ylabel="True positive rate",
-    roc_color="blue",
-    reference_color="black",
+    y,
+    y_pred,
+    title=None,
+    xlabel=None,
+    ylabel=None,
     savefig=False,
-    figname=None,
+    fname=None,
+    **kwargs
 ):
-    """Plot AUROC for dataframe with probabilities and binary target.
+    """Plot AUC ROC curve for arrays with predicted probabilities and labels.
 
     Parameters
     ----------
-    df : pandas.DataFrame
-        Dataframe with the columns to be plotted.
+    y : numpy.array or pandas.Series
+        Array with the target labels.
 
-    target_col : str
-        Column with binary target.
+    y_pred : numpy.array or pandas.Series
+        Array with predicted probabilities.
 
-    proba_col : str
-        Column with the calculated probabilites.
-
-    title : str (default="ROC curve")
+    title : str (default=None)
         Title for plot.
 
-    xlabel : str (default="False positive rate")
+    xlabel : str (default=None)
         Label for the x-axis.
 
-    ylabel : str (default="True positive rate")
+    ylabel : str (default=None)
         Label for the y-axis.
-
-    roc_color : str (default="blue")
-        Color for the roc line.
-
-    reference_color : str (default="red")
-        Color for the reference line.
 
     savefig : bool (default=False)
         To save figure.
 
-    figname : str (default=None)
+    fname : str (default=None)
         Name for figure file.
 
-    Returns
-    -------
-    auroc : float
-        Compute Area Under the Receiver Operating Characteristic Curve
-        (ROC AUC) from prediction scores.
+    **kwargs : keyword arguments
+        Keyword arguments for matplotlib.pyplot.savefig().
+
     """
+    y, y_pred = _check_arrays(y, y_pred)
+
+    _check_parameters(title, xlabel, ylabel, savefig, fname)
+
     # Define the arrays for plotting
-    fpr, tpr, threshold = roc_curve(df[target_col], df[proba_col])
+    fpr, tpr, _ = roc_curve(y, y_pred)
+    auc_roc = roc_auc_score(y, y_pred)
 
     # Define the plot settings
-    plt.plot(fpr, tpr, color=roc_color)
-    plt.plot(fpr, fpr, linestyle="--", color=reference_color)
-    plt.title(title, fontdict={"fontsize": 16})
-    plt.xlabel(xlabel, fontdict={"fontsize": 16})
-    plt.ylabel(ylabel, fontdict={"fontsize": 16})
+    if title is None:
+        title = "ROC curve"
+    if xlabel is None:
+        xlabel = "False Positive Rate"
+    if ylabel is None:
+        ylabel = "True Positive Rate"
 
+    plt.plot(fpr, fpr, linestyle="--", color="k", label="Random Model")
+    plt.plot(fpr, tpr, color="g", label="Model (AUC: {:.5f})".format(auc_roc))
+    plt.title(title, fontdict={"fontsize": 14})
+    plt.xlabel(xlabel, fontdict={"fontsize": 12})
+    plt.ylabel(ylabel, fontdict={"fontsize": 12})
+    plt.legend(loc='lower right')
+
+    # Save figure if requested. Pass kwargs.
     if savefig:
-        plt.savefig(figname, dpi=300)
-
-    # Set AUROC score
-    auroc = roc_auc_score(df[target_col], df[proba_col])
-    return auroc
+        plt.savefig(fname=fname, **kwargs)
 
 
-def plot_gini(
-    df,
-    target_col,
-    proba_col,
-    title="Gini",
-    xlabel="Cumulative % Population",
-    ylabel="Cumulative % Bad",
-    gini_color="blue",
-    reference_color="black",
+def plot_cap(
+    y,
+    y_pred,
+    title=None,
+    xlabel=None,
+    ylabel=None,
     savefig=False,
-    figname=None,
+    fname=None,
+    **kwargs
 ):
-    """Plot Gini for dataframe with probabilities and binary target.
+    """Plot Cumulative Accuracy Profile (CAP) for given arrays.
 
     Parameters
     ----------
+    y : numpy.array or pandas.Series
+        Array with the target labels.
 
-    df : pandas.DataFrame
-        Dataframe with the columns to be plotted.
+    y_pred : numpy.array or pandas.Series
+        Array with predicted probabilities.
 
-    target_col : str
-        Column with binary target.
-
-    proba_col : str
-        Column with the calculated probabilites.
-
-    title : str (default="Gini"):
+    title : str (default=None)
         Title for plot.
 
-    xlabel : str (default="Cumulative % Population")
+    xlabel : str (default=None)
         Label for the x-axis.
 
-    ylabel : str (default="Cumulative % Bad")
+    ylabel : str (default=None)
         Label for the y-axis.
-
-    roc_color : str (default="blue")
-        Color for the Gini line.
-
-    reference_color : str (default="red")
-        Color for the reference line.
 
     savefig : bool (default=False)
         To save figure.
 
-    figname : str (default=None)
+    fname : str (default=None)
         Name for figure file.
 
-    Returns
-    -------
+    **kwargs : keyword arguments
+        Keyword arguments for matplotlib.pyplot.savefig().
 
-    gini : float
-        Calculate Gini Index.
     """
-    # Sort dataframe by probabilities and reset index
-    df = df.sort_values(proba_col)
-    df = df.reset_index()
+    y, y_pred = _check_arrays(y, y_pred)
 
-    # Calculate cumulative columns
-    df["cumulative_n_population"] = df.index + 1
-    df["cumulative_n_good"] = df[target_col].cumsum()
-    df["cumulative_n_bad"] = (
-        df["cumulative_n_population"] - df[target_col].cumsum()
-    )
-    df["cumulative_perc_population"] = df["cumulative_n_population"] / (
-        df.shape[0]
-    )
-    df["cumulative_perc_good"] = df["cumulative_n_good"] / df[target_col].sum()
-    df["cumulative_perc_bad"] = df["cumulative_n_bad"] / (
-        df.shape[0] - df[target_col].sum()
-    )
+    _check_parameters(title, xlabel, ylabel, savefig, fname)
 
-    # Plot Gini
-    plt.plot(
-        df["cumulative_perc_population"],
-        df["cumulative_perc_bad"],
-        color=gini_color,
-    )
-    plt.plot(
-        df["cumulative_perc_population"],
-        df["cumulative_perc_population"],
-        linestyle="--",
-        color=reference_color,
-    )
-    plt.title(title, fontdict={"fontsize": 16})
-    plt.xlabel(xlabel, fontdict={"fontsize": 16})
-    plt.ylabel(ylabel, fontdict={"fontsize": 16})
+    n_samples = y.shape[0]
+    n_event = np.sum(y)
 
-    if savefig:
-        plt.savefig(figname, dpi=300)
+    idx = y_pred.argsort()[::-1][:n_samples]
+    yy = y[idx]
 
-    # Calculate Gini Index
-    auroc = roc_auc_score(df[target_col], df[proba_col])
+    p_event = np.append([0], np.cumsum(yy)) / n_event
+    p_population = np.arange(0, n_samples + 1) / n_samples
+
+    auroc = roc_auc_score(y, y_pred)
     gini = auroc * 2 - 1
-    return gini
+
+    # Define the plot settings
+    if title is None:
+        title = "Cumulative Accuracy Profile (CAP)"
+    if xlabel is None:
+        xlabel = "Fraction of all population"
+    if ylabel is None:
+        ylabel = "Fraction of event population"
+
+    plt.plot([0, 1], [0, 1], color='k', linestyle='--', label="Random Model")
+    plt.plot(
+        [0, n_event / n_samples, 1],
+        [0, 1, 1],
+        color='grey',
+        linestyle='--',
+        label="Perfect Model",
+    )
+    plt.plot(
+        p_population,
+        p_event,
+        color="g",
+        label="Model (Gini: {:.5f})".format(gini),
+    )
+
+    plt.title(title, fontdict={'fontsize': 14})
+    plt.xlabel(xlabel, fontdict={'fontsize': 12})
+    plt.ylabel(ylabel, fontdict={'fontsize': 12})
+    plt.legend(loc='lower right')
+
+    # Save figure if requested. Pass kwargs.
+    if savefig:
+        plt.savefig(fname=fname, **kwargs)
 
 
 def plot_ks(
-    df,
-    target_col,
-    proba_col,
-    title="Kolmogorov-Smirnov",
-    xlabel="Estimated Probability for target Good",
-    ylabel="Cumulative %",
-    good_color="blue",
-    bad_color="red",
+    y,
+    y_pred,
+    title=None,
+    xlabel=None,
+    ylabel=None,
     savefig=False,
-    figname=None,
+    fname=None,
+    **kwargs
 ):
-    """Create Kolmogorov-Smirnov plot for probabilities and binary target.
+    """Plot Kolmogorov-Smirnov for given arrays.
 
     Parameters
     ----------
-    df : pandas.DataFrame
-        Dataframe with the columns to be plotted.
+    y : numpy.array or pandas.Series
+        Array with the target labels.
 
-    target_col : str
-        Column with binary target.
+    y_pred : numpy.array or pandas.Series
+        Array with predicted probabilities.
 
-    proba_col : str
-        Column with the calculated probabilites.
-
-    title : str (default="Kolmogorov-Smirnov"):
+    title : str (default=None)
         Title for plot.
 
-    xlabel : str (default="Estimated Probability for target Good")
+    xlabel : str (default=None)
         Label for the x-axis.
 
-    ylabel : str (default="Cumulative %")
+    ylabel : str (default=None)
         Label for the y-axis.
-
-    good_color : str (default="blue")
-        Color for the event lineplot.
-
-    bad_color : str (default="red")
-        Color for the non-event lineplot.
 
     savefig : bool (default=False)
         To save figure.
 
-    figname : str (default=None)
+    fname : str (default=None)
         Name for figure file.
 
-    Returns
-    -------
-    ks_score : float
-        Calculate KS Score.
+    **kwargs : keyword arguments
+        Keyword arguments for matplotlib.pyplot.savefig().
+
     """
-    # Sort dataframe by probabilities and reset index
-    df = df.sort_values(proba_col)
-    df = df.reset_index()
+    y, y_pred = _check_arrays(y, y_pred)
 
-    # Calculate cumulative columns
-    df["cumulative_n_population"] = df.index + 1
-    df["cumulative_n_good"] = df[target_col].cumsum()
-    df["cumulative_n_bad"] = (
-        df["cumulative_n_population"] - df[target_col].cumsum()
-    )
-    df["cumulative_perc_population"] = df["cumulative_n_population"] / (
-        df.shape[0]
-    )
-    df["cumulative_perc_good"] = df["cumulative_n_good"] / df[target_col].sum()
-    df["cumulative_perc_bad"] = df["cumulative_n_bad"] / (
-        df.shape[0] - df[target_col].sum()
-    )
+    _check_parameters(title, xlabel, ylabel, savefig, fname)
 
-    # Plot KS
-    plt.plot(
-        df[proba_col], df["cumulative_perc_bad"], color=bad_color,
-    )
-    plt.plot(
-        df[proba_col], df["cumulative_perc_good"], color=good_color,
-    )
-    plt.xlabel(
-        "Estimated Probability for Good target", fontdict={"fontsize": 16}
-    )
-    plt.ylabel("Cumulative %", fontdict={"fontsize": 16})
-    plt.title("Kolmogorov-Smirnov", fontdict={"fontsize": 16})
+    n_samples = y.shape[0]
+    n_event = np.sum(y)
+    n_nonevent = n_samples - n_event
 
-    ks_score = max(df["cumulative_perc_bad"] - df["cumulative_perc_good"])
-    ks_max_idx = np.argmax(
-        df["cumulative_perc_bad"] - df["cumulative_perc_good"]
-    )
+    idx = y_pred.argsort()
+    yy = y[idx]
+    pp = y_pred[idx]
 
-    # Define the KS line
+    cum_event = np.cumsum(yy)
+    cum_population = np.arange(0, n_samples)
+    cum_nonevent = cum_population - cum_event
+
+    p_event = cum_event / n_event
+    p_nonevent = cum_nonevent / n_nonevent
+
+    p_diff = p_nonevent - p_event
+    ks_score = np.max(p_diff)
+    ks_max_idx = np.argmax(p_diff)
+
+    # Define the plot settings
+    if title is None:
+        title = "Kolmogorov-Smirnov"
+    if xlabel is None:
+        xlabel = "Threshold"
+    if ylabel is None:
+        ylabel = "Cumulative probability"
+
+    plt.title(title, fontdict={'fontsize': 14})
+    plt.xlabel(xlabel, fontdict={'fontsize': 12})
+    plt.ylabel(ylabel, fontdict={'fontsize': 12})
+
+    plt.plot(pp, p_event, color="r", label="Cumulative events")
+    plt.plot(pp, p_nonevent, color="b", label="Cumulative non-events")
     plt.vlines(
-        df[proba_col].iloc[ks_max_idx],
-        ymin=df["cumulative_perc_good"].iloc[ks_max_idx],
-        ymax=df["cumulative_perc_bad"].iloc[ks_max_idx],
-        color="black",
+        pp[ks_max_idx],
+        ymin=p_event[ks_max_idx],
+        ymax=p_nonevent[ks_max_idx],
+        color="k",
         linestyles="--",
     )
 
     # Set KS value inside plot
-    plt.text(
-        df[proba_col].iloc[ks_max_idx] + 0.1,
-        (
-            (
-                df["cumulative_perc_good"].iloc[ks_max_idx]
-                + df["cumulative_perc_bad"].iloc[ks_max_idx]
-            )
-            * 0.5
-        ),
-        f"KS = {round(ks_score * 100, 2)}%",
-        fontsize=12,
-        rotation_mode="anchor",
-    )
+    pos_x = pp[ks_max_idx] + 0.02
+    pos_y = 0.5 * (p_nonevent[ks_max_idx] + p_event[ks_max_idx])
+    text = "KS: {:.2%} at {:.2f}".format(ks_score, pp[ks_max_idx])
+    plt.text(pos_x, pos_y, text, fontsize=12, rotation_mode="anchor")
 
+    plt.legend(loc='lower right')
+
+    # Save figure if requested. Pass kwargs.
     if savefig:
-        plt.savefig(figname, dpi=300)
-
-    return ks_score
+        plt.savefig(fname=fname, **kwargs)

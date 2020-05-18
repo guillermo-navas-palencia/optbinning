7bcf8950a2b7671d9cf087af38f2915d477198fa
GabrielSGoncalves <gabrielgoncalvesbr@gmail.com>
2020-05-18 10:06:32 -0300

    Change plots.py function names.

--- optbinning/scorecard/__init__.py
+++ optbinning/scorecard/__init__.py
@@ -1,4 +1,4 @@
 from .scorecard import Scorecard
-from .plots import plot_auroc, plot_gini, plot_ks
+from .plots import plot_auc_roc, plot_cap, plot_ks
 
-__all__ = ["Scorecard", "plot_auroc", "plot_gini", "plot_ks"]
+__all__ = ["Scorecard", "plot_auc_roc", "plot_cap", "plot_ks"]

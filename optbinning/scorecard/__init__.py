67149b3f7e83bea520d5096866dc92cb540b095e
GUI <g.navas.palencia@gmail.com>
2020-09-13 20:30:11 +0200

    Add ScorecardMonitoring class to __init__. Add class to scorecard section in docs

--- optbinning/scorecard/__init__.py
+++ optbinning/scorecard/__init__.py
@@ -1,8 +1,10 @@
 from .scorecard import Scorecard
 from .plots import plot_auc_roc, plot_cap, plot_ks
+from .monitoring import ScorecardMonitoring
 
 
 __all__ = ["Scorecard",
+           "ScorecardMonitoring",
            "plot_auc_roc",
            "plot_cap",
            "plot_ks"]

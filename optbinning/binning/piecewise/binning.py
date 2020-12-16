1ced667d8eed42d8619c2c2f75593c70d16d2507
GUI <g.navas.palencia@gmail.com>
2020-12-16 15:44:14 +0100

    OptimalPWBinning init docstring

--- optbinning/binning/piecewise/binning.py
+++ optbinning/binning/piecewise/binning.py
@@ -18,10 +18,151 @@ from .transformations import transform_binary_target
 
 
 class OptimalPWBinning(BasePWBinning):
-    """Optimal Piecewise binning.
+    """Optimal Piecewise binning of a numerical variable with respect to a
+    binary target.
 
     Parameters
     ----------
+    name : str, optional (default="")
+        The variable name.
+
+    estimator : object or None (default=None)
+        An esimator to compute probability estimates. If None, it uses
+        `sklearn.linear_model.LogisticRegression
+        <https://scikit-learn.org/stable/modules/generated/
+        sklearn.linear_model.LogisticRegression.html>`_. The estimator must be
+        an object with method `predict_proba`.
+
+    objective : str, optional (default="l2")
+        The objective function. Supported objectives are "l2", "l1", "huber"
+        and "quantile". Note that "l1", "huber" and "quantile" are robust
+        objective functions.
+
+    degree : int (default=1)
+        The degree of the polynomials.
+
+        * degree = 0: piecewise constant functions.
+        * degree = 1: piecewise linear functions.
+        * degree > 1: piecewise polynomial functions.
+
+    continuous : bool (default=True)
+        Whether to fit a continuous or discontinuous piecewise regression.
+
+    prebinning_method : str, optional (default="cart")
+        The pre-binning method. Supported methods are "cart" for a CART
+        decision tree, "quantile" to generate prebins with approximately same
+        frequency and "uniform" to generate prebins with equal width. Method
+        "cart" uses `sklearn.tree.DecistionTreeClassifier
+        <https://scikit-learn.org/stable/modules/generated/sklearn.tree.
+        DecisionTreeClassifier.html>`_.
+
+    max_n_prebins : int (default=20)
+        The maximum number of bins after pre-binning (prebins).
+
+    min_prebin_size : float (default=0.05)
+        The fraction of mininum number of records for each prebin.
+
+    min_n_bins : int or None, optional (default=None)
+        The minimum number of bins. If None, then ``min_n_bins`` is
+        a value in ``[0, max_n_prebins]``.
+
+    max_n_bins : int or None, optional (default=None)
+        The maximum number of bins. If None, then ``max_n_bins`` is
+        a value in ``[0, max_n_prebins]``.
+
+    min_bin_size : float or None, optional (default=None)
+        The fraction of minimum number of records for each bin. If None,
+        ``min_bin_size = min_prebin_size``.
+
+    max_bin_size : float or None, optional (default=None)
+        The fraction of maximum number of records for each bin. If None,
+        ``max_bin_size = 1.0``.
+
+    monotonic_trend : str or None, optional (default="auto")
+        The **event rate** monotonic trend. Supported trends are “auto”,
+        "auto_heuristic" and "auto_asc_desc" to automatically determine the
+        trend maximizing IV using a machine learning classifier, "ascending",
+        "descending", "concave", "convex", "peak" and "peak_heuristic" to allow
+        a peak change point, and "valley" and "valley_heuristic" to allow a
+        valley change point. Trends "auto_heuristic", "peak_heuristic" and
+        "valley_heuristic" use a heuristic to determine the change point,
+        and are significantly faster for large size instances (``max_n_prebins
+        > 20``). Trend "auto_asc_desc" is used to automatically select the best
+        monotonic trend between "ascending" and "descending". If None, then the
+        monotonic constraint is disabled.
+
+    n_subsamples : int or None (default=None)
+        Number of subsamples to fit the piecewise regression algorithm. If
+        None, all values are considered.
+
+    max_pvalue : float or None, optional (default=0.05)
+        The maximum p-value among bins. The Z-test is used to detect bins
+        not satisfying the p-value constraint. Option supported by solvers
+        "cp" and "mip".
+
+    max_pvalue_policy : str, optional (default="consecutive")
+        The method to determine bins not satisfying the p-value constraint.
+        Supported methods are "consecutive" to compare consecutive bins and
+        "all" to compare all bins.
+
+    outlier_detector : str or None, optional (default=None)
+        The outlier detection method. Supported methods are "range" to use
+        the interquartile range based method or "zcore" to use the modified
+        Z-score method.
+
+    outlier_params : dict or None, optional (default=None)
+        Dictionary of parameters to pass to the outlier detection method.
+
+    user_splits : array-like or None, optional (default=None)
+        The list of pre-binning split points when ``dtype`` is "numerical" or
+        the list of prebins when ``dtype`` is "categorical".
+
+    user_splits_fixed : array-like or None (default=None)
+        The list of pre-binning split points that must be fixed.
+
+    special_codes : array-like or None, optional (default=None)
+        List of special codes. Use special codes to specify the data values
+        that must be treated separately.
+
+    split_digits : int or None, optional (default=None)
+        The significant digits of the split points. If ``split_digits`` is set
+        to 0, the split points are integers. If None, then all significant
+        digits in the split points are considered.
+
+    solver : str, optional (default="auto")
+        The optimizer to solve the underlying mathematical optimization
+        problem. Supported solvers are `"ecos"
+        <https://github.com/embotech/ecos>`_, `"osqp"
+        <https://github.com/oxfordcontrol/osqp>`_, "direct", to choose the
+        direct solver, and "auto", to choose the most appropriate solver for
+        the problem.
+
+    h_epsilon: float (default=1.35)
+        The parameter h_epsilon used when ``objective="huber"``, controls the
+        number of samples that should be classified as outliers.
+
+    quantile : float (default=0.5)
+        The parameter quantile is the q-th quantile to be used when
+        ``objective="quantile"``.
+
+    regularization: str or None (default=None)
+        Type of regularization. Supported regularization are "l1" (Lasso) and
+        "l2" (Ridge). If None, no regularization is applied.
+
+    reg_l1 : float (default=1.0)
+        L1 regularization term. Increasing this value will smooth the
+        regression model. Only applicable if ``regularization="l1"``.
+
+    reg_l2 : float (default=1.0)
+        L2 regularization term. Increasing this value will smooth the
+        regression model. Only applicable if ``regularization="l2"``.
+
+    random_state : int, RandomState instance or None, (default=None)
+        If ``n_subsamples < n_samples``, controls the shuffling applied to the
+        data before applying the split.
+
+    verbose : bool (default=False)
+        Enable verbose output.
     """
     def __init__(self, name="", estimator=None, objective="l2", degree=1,
                  continuous=True, prebinning_method="cart", max_n_prebins=20,

4b7a74a743d2f9ced13f01119e9c778568167cc2
GUI <g.navas.palencia@gmail.com>
2021-01-12 20:29:49 +0100

    Update copyright date

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- Project information -----------------------------------------------------
 
 project = 'optbinning'
-copyright = '2019 - 2020, Guillermo Navas-Palencia'
+copyright = '2019 - 2021, Guillermo Navas-Palencia'
 author = 'Guillermo Navas-Palencia'
 
 # The short X.Y version

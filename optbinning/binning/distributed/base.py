cdec69b3959d499b897f0b8dd5bba22183d42125
GUI <g.navas.palencia@gmail.com>
2021-04-23 19:40:44 +0200

    Implementation of BaseSketch class

---
+++ optbinning/binning/distributed/base.py
@@ -0,0 +1,29 @@
+"""
+Base optimal binning sketch algorithm class.
+"""
+
+# Guillermo Navas-Palencia <g.navas.palencia@gmail.com>
+# Copyright (C) 2021
+
+from sklearn.exceptions import NotFittedError
+
+from ...logging import Logger
+
+
+class BaseSketch:
+    def __getstate__(self):
+        d = self.__dict__.copy()
+        del d["_logger"]
+        del d["_class_logger"]
+        return d
+
+    def __setstate__(self, d):
+        self.__dict__.update(d)
+        self._class_logger = Logger(__name__)
+        self._logger = self._class_logger.logger
+
+    def _check_is_solved(self):
+        if not self._is_solved:
+            raise NotFittedError("This {} instance is not solved yet. Call "
+                                 "'solve' with appropriate arguments."
+                                 .format(self.__class__.__name__))

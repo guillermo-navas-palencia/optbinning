a6bea2fd17631357dffc89ea7050c75b3ffb62ab
GUI <g.navas.palencia@gmail.com>
2021-05-20 19:15:43 +0200

    Add docstring on custom error and warning exceptions

--- optbinning/exceptions.py
+++ optbinning/exceptions.py
@@ -7,16 +7,33 @@ Custom error and warning exceptions.
 
 
 class NotDataAddedError(ValueError, AttributeError):
-    """"""
+    """Exception class to raise if binning sketch is solved before adding
+    data.
 
-
-class NotGeneratedError(ValueError, AttributeError):
-    """"""
+    This class inherits from both ValueError and AttributeError to help with
+    exception handling and backward compatibility.
+    """
 
 
 class NotSolvedError(ValueError, AttributeError):
-    """"""
+    """Exception class to raise if binning sketch methods are called before
+    solving.
+
+    This class inherits from both ValueError and AttributeError to help with
+    exception handling and backward compatibility.
+    """
+
+
+class NotGeneratedError(ValueError, AttributeError):
+    """Exception class to raise is counterfactual information is requested
+    before generating explanations.
+
+    This class inherits from both ValueError and AttributeError to help with
+    exception handling and backward compatibility.
+    """
 
 
 class CounterfactualsFoundWarning(UserWarning):
-    """"""
+    """Warning used to notify no feasible counterfactual explanations were
+    found.
+    """

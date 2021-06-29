7a8ab9d5a6c6c518c9c3027e0aaf65702d2ad032
GUI <g.navas.palencia@gmail.com>
2021-06-29 18:01:51 +0200

    Update unit test scorecard X y

--- tests/test_scorecard_monitoring.py
+++ tests/test_scorecard_monitoring.py
@@ -30,98 +30,92 @@ def _data(target_dtype):
         data = load_boston()
 
     variable_names = data.feature_names
-    df = pd.DataFrame(data.data, columns=variable_names)
-    df["target"] = data.target
+    X = pd.DataFrame(data.data, columns=variable_names)
+    y = data.target
 
-    df_train, df_test = train_test_split(df, test_size=0.3, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42)
 
-    return df_train, df_test
+    return X_train, y_train, X_test, y_test
 
 
-def _fit_scorecard(target_dtype, df_train):
+def _fit_scorecard(target_dtype, X_train, y_train):
     if target_dtype == "binary":
         estimator = LogisticRegression()
     else:
         estimator = LinearRegression()
 
-    variable_names = list(df_train.columns)
+    variable_names = list(X_train.columns)
     binning_process = BinningProcess(variable_names)
-    scorecard = Scorecard(target="target", binning_process=binning_process,
-                          estimator=estimator).fit(df_train)
+    scorecard = Scorecard(binning_process=binning_process,
+                          estimator=estimator).fit(X_train, y_train)
 
     return scorecard
 
 
 def test_params():
-    df_train, df_test = _data("binary")
-    scorecard = _fit_scorecard("binary", df_train)
+    X_train, y_train, X_test, y_test = _data("binary")
+    scorecard = _fit_scorecard("binary", X_train, y_train)
 
     with raises(TypeError):
-        monitoring = ScorecardMonitoring(target=1, scorecard=scorecard)
-        monitoring.fit(df_test, df_train)
-
-    with raises(TypeError):
-        monitoring = ScorecardMonitoring(target="target", scorecard=None)
-        monitoring.fit(df_test, df_train)
+        monitoring = ScorecardMonitoring(scorecard=None)
+        monitoring.fit(X_test, y_test, X_train, y_train)
 
     with raises(ValueError):
-        monitoring = ScorecardMonitoring(target="target", scorecard=scorecard,
+        monitoring = ScorecardMonitoring(scorecard=scorecard,
                                          psi_method="new_method")
-        monitoring.fit(df_test, df_train)
+        monitoring.fit(X_test, y_test, X_train, y_train)
 
     with raises(ValueError):
-        monitoring = ScorecardMonitoring(target="target", scorecard=scorecard,
-                                         psi_n_bins=-12.2)
-        monitoring.fit(df_test, df_train)
+        monitoring = ScorecardMonitoring(scorecard=scorecard, psi_n_bins=-12.2)
+        monitoring.fit(X_test, y_test, X_train, y_train)
 
     with raises(ValueError):
-        monitoring = ScorecardMonitoring(target="target", scorecard=scorecard,
+        monitoring = ScorecardMonitoring(scorecard=scorecard,
                                          psi_min_bin_size=0.8)
-        monitoring.fit(df_test, df_train)
+        monitoring.fit(X_test, y_test, X_train, y_train)
 
     with raises(ValueError):
-        monitoring = ScorecardMonitoring(target="target", scorecard=scorecard,
-                                         show_digits=9)
-        monitoring.fit(df_test, df_train)
+        monitoring = ScorecardMonitoring(scorecard=scorecard, show_digits=9)
+        monitoring.fit(X_test, y_test, X_train, y_train)
 
     with raises(TypeError):
-        monitoring = ScorecardMonitoring(target="target", scorecard=scorecard,
-                                         verbose=1)
-        monitoring.fit(df_test, df_train)
+        monitoring = ScorecardMonitoring(scorecard=scorecard, verbose=1)
+        monitoring.fit(X_test, y_test, X_train, y_train)
 
 
 def test_input():
-    df_train, df_test = _data("binary")
-    scorecard = _fit_scorecard("binary", df_train)
-    monitoring = ScorecardMonitoring(target="target", scorecard=scorecard)
+    X_train, y_train, X_test, y_test = _data("binary")
+    scorecard = _fit_scorecard("binary", X_train, y_train)
+    monitoring = ScorecardMonitoring(scorecard=scorecard)
 
     with raises(ValueError):
-        df_test_2 = df_test.copy()
-        columns = list(df_test.columns)
+        X_test_2 = X_test.copy()
+        columns = list(X_test.columns)
         columns[3] = columns[3].upper()
-        df_test_2.columns = columns
-        monitoring.fit(df_test_2, df_train)
+        X_test_2.columns = columns
+        monitoring.fit(X_test_2, y_test, X_train, y_train)
 
     with raises(ValueError):
-        df_test_2 = df_test.copy()
-        df_test_2["target"] = np.random.randint(0, 3, len(df_test))
-        monitoring.fit(df_test_2, df_train)
+        X_test_2 = X_test.copy()
+        y_test_2 = np.random.randint(0, 3, len(X_test))
+        monitoring.fit(X_test_2, y_test_2, X_train, y_train)
 
     with raises(ValueError):
-        df_train["target"] = np.random.randint(0, 3, len(df_train))
-        monitoring.fit(df_test, df_train)
+        y_train_2 = np.random.randint(0, 3, len(X_train))
+        monitoring.fit(X_test, y_test, X_train, y_train_2)
 
     with raises(ValueError):
-        df_train["target"] = np.random.randn(len(df_train))
-        monitoring.fit(df_test, df_train)
+        y_train_2 = np.random.randn(len(X_train))
+        monitoring.fit(X_test, y_test, X_train, y_train_2)
 
 
 def test_default_binary():
-    df_train, df_test = _data("binary")
-    scorecard = _fit_scorecard("binary", df_train)
+    X_train, y_train, X_test, y_test = _data("binary")
+    scorecard = _fit_scorecard("binary", X_train, y_train)
 
-    monitoring = ScorecardMonitoring(target="target", scorecard=scorecard)
-    monitoring.fit(df_test, df_train)
+    monitoring = ScorecardMonitoring(scorecard=scorecard)
+    monitoring.fit(X_test, y_test, X_train, y_train)
 
     # Check psi_table
     psi_table = monitoring.psi_table()
@@ -149,15 +143,15 @@ def test_default_binary():
 
 
 def test_default_continuous():
-    df_train, df_test = _data("continuous")
-    scorecard = _fit_scorecard("continuous", df_train)
+    X_train, y_train, X_test, y_test = _data("continuous")
+    scorecard = _fit_scorecard("continuous", X_train, y_train)
 
-    monitoring = ScorecardMonitoring(target="target", scorecard=scorecard)
-    monitoring.fit(df_test, df_train)
+    monitoring = ScorecardMonitoring(scorecard=scorecard)
+    monitoring.fit(X_test, y_test, X_train, y_train)
 
     # Check psi_table
     psi_table = monitoring.psi_table()
-    assert psi_table.PSI.sum() == approx(0.24101307906596886)
+    assert psi_table.PSI.sum() == approx(0.39483122757230243)
 
     # Check psi_variable_table
     assert monitoring.psi_variable_table(
@@ -177,12 +171,12 @@ def test_default_continuous():
 
     # Check psi splits
     assert monitoring.psi_splits[:4] == approx(
-        [11.936903, 15.63085079, 18.19052601, 19.64347458], rel=1e-4)
+        [27.18795586, 29.80329227, 31.72756577, 33.10686493], rel=1e-4)
 
     # Check tests table
     tests_table = monitoring.tests_table()
     assert tests_table["p-value"].values[:2] == approx(
-        [0.80810407, 0.69939868], rel=1e-4)
+        [0.595254, 0.006365], rel=1e-4)
 
     # Check system stability report
     with open("tests/test_scorecard_monitoring_default_continuous.txt",
@@ -192,10 +186,10 @@ def test_default_continuous():
 
 
 def test_information():
-    df_train, df_test = _data("continuous")
-    scorecard = _fit_scorecard("continuous", df_train)
+    X_train, y_train, X_test, y_test = _data("continuous")
+    scorecard = _fit_scorecard("continuous", X_train, y_train)
 
-    monitoring = ScorecardMonitoring(target="target", scorecard=scorecard)
+    monitoring = ScorecardMonitoring(scorecard=scorecard)
 
     with raises(NotFittedError):
         monitoring.information()
@@ -203,7 +197,7 @@ def test_information():
     with raises(NotFittedError):
         monitoring.psi_splits
 
-    monitoring.fit(df_test, df_train)
+    monitoring.fit(X_test, y_test, X_train, y_train)
 
     with raises(ValueError):
         monitoring.information(print_level=-1)
@@ -216,23 +210,22 @@ def test_information():
 
 
 def test_verbose():
-    df_train, df_test = _data("continuous")
-    scorecard = _fit_scorecard("continuous", df_train)
+    X_train, y_train, X_test, y_test = _data("continuous")
+    scorecard = _fit_scorecard("continuous", X_train, y_train)
 
-    monitoring = ScorecardMonitoring(target="target", scorecard=scorecard,
-                                     verbose=True)
+    monitoring = ScorecardMonitoring(scorecard=scorecard, verbose=True)
 
     with open("tests/test_scorecard_monitoring_verbose.txt", "w") as f:
         with redirect_stdout(f):
-            monitoring.fit(df_test, df_train)
+            monitoring.fit(X_test, y_test, X_train, y_train)
 
 
 def test_plot_binary():
-    df_train, df_test = _data("binary")
-    scorecard = _fit_scorecard("binary", df_train)
+    X_train, y_train, X_test, y_test = _data("binary")
+    scorecard = _fit_scorecard("binary", X_train, y_train)
 
-    monitoring = ScorecardMonitoring(target="target", scorecard=scorecard)
-    monitoring.fit(df_test, df_train)
+    monitoring = ScorecardMonitoring(scorecard=scorecard)
+    monitoring.fit(X_test, y_test, X_train, y_train)
 
     with raises(TypeError):
         monitoring.psi_plot(savefig=1)
@@ -241,9 +234,9 @@ def test_plot_binary():
 
 
 def test_plot_continuous():
-    df_train, df_test = _data("continuous")
-    scorecard = _fit_scorecard("continuous", df_train)
+    X_train, y_train, X_test, y_test = _data("continuous")
+    scorecard = _fit_scorecard("continuous", X_train, y_train)
 
-    monitoring = ScorecardMonitoring(target="target", scorecard=scorecard)
-    monitoring.fit(df_test, df_train)
+    monitoring = ScorecardMonitoring(scorecard=scorecard)
+    monitoring.fit(X_test, y_test, X_train, y_train)
     monitoring.psi_plot(savefig="psi_plot_continuous.png")

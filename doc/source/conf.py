e40f73b6ba0f39f32e2248aa8ba2a80732fe6594
GUI <g.navas.palencia@gmail.com>
2023-09-22 19:33:29 +0200

    Update version to 0.18.0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,9 +22,9 @@ copyright = '2019 - 2023, Guillermo Navas-Palencia'
 author = 'Guillermo Navas-Palencia'
 
 # The short X.Y version
-version = '0.17.3'
+version = '0.18.0'
 # The full version, including alpha/beta/rc tags
-release = '0.17.3'
+release = '0.18.0'
 
 
 # -- General configuration ---------------------------------------------------

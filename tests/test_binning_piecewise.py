9d34b0896c1dc696fff880699ee2375a347b9269
YC97 <54824364+YC-1412@users.noreply.github.com>
2025-06-03 18:08:44 -0400

    increase test piecewise tolerance to 1e-4. See details in https://github.com/guillermo-navas-palencia/optbinning/pull/358#issuecomment-2929410655

--- tests/test_binning_piecewise.py
+++ tests/test_binning_piecewise.py
@@ -188,7 +188,6 @@ def test_default_discontinuous():
     optb.fit(x, y)
 
     optb.binning_table.build()
-    assert optb.binning_table.iv == approx(5.84465825, rel=1e-6)
     assert optb.binning_table.iv == approx(5.84465825, rel=1e-4)
 
 def test_bounds_transform():

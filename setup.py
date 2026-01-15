de4bbeead5b359f4aed2794b8142509ea04a5bfb
Justin Mahlik <justin.mahlik.rm13@statefarm.com>
2026-01-15 12:32:16 -0600

    fix(dependencies): allow ortools <9.16
    Exclude ortools 9.12 because it has potential bugs and does not pass
    tests. All other allowed versions of ortools are passing.
    
    9.12 was never supported and shouldn't require a separate path through
    the code. Just exclude it instead.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class CleanCommand(Command):
 install_requires = [
     'matplotlib',
     'numpy>=1.16.1',
-    'ortools>=9.4,<9.15',
+    'ortools>=9.4,<9.16,!=9.12.4544',
     'pandas',
     'ropwr>=1.0.0',
     'scikit-learn>=1.6.0',
